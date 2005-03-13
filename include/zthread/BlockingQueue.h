cf323d129c6b255e814718011b04926f2d8d387b
ecrahen <ecrahen>
2005-03-13 13:17:31 +0000

    *** empty log message ***

--- include/zthread/BlockingQueue.h
+++ include/zthread/BlockingQueue.h
@@ -83,7 +83,7 @@ namespace ZThread {
       /**
        * @see Queue::add(const T& item, unsigned long timeout)
        */
-      virtual bool add(T item, unsigned long timeout) {
+      virtual bool add(const T& item, unsigned long timeout) {
 
         try {
 
